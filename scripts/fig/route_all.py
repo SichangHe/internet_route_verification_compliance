f5e1fec620bae11e36a3c6fde393b113bb19b5cd
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-11-11 10:39:05 +0800

    route with all reports in one type

---
+++ scripts/fig/route_all.py
@@ -0,0 +1,42 @@
+"""Run at `scripts/` with `python3 -m fig.route_all`.
+Data are from here:
+<https://github.com/SichangHe/internet_route_verification/issues/88>
+"""
+import pandas as pd
+from fig import download_if_missing
+
+FILE = "route_stats1.csv.gz"
+PORTS = ("import", "export")
+TAGS = ("ok", "skip", "unrec", "meh", "err")
+
+
+def main():
+    download_if_missing(
+        "https://github.com/SichangHe/internet_route_verification/releases/download/data-88/route_stats1.csv.gz",
+        FILE,
+    )
+    df = pd.read_csv(FILE, dtype="uint16")
+    n_route = len(df)
+    df["total"] = sum((df[f"{port}_{tag}"] for tag in TAGS for port in PORTS))
+    df_all = {}
+    for tag in TAGS:
+        df_all[tag] = df[
+            df[f"import_{tag}"] + df[f"export_{tag}"] == df["total"]
+        ].dropna()
+        count = df_all[tag].__len__()
+        percentage = count / n_route * 100
+        print(f"{count} all {tag}, {percentage}%.")
+    for port in PORTS:
+        print()
+        df[f"total_{port}"] = sum((df[f"{port}_{tag}"] for tag in TAGS))
+        for tag in TAGS:
+            df_all[f"{port}_{tag}"] = df[
+                df[f"{port}_{tag}"] == df[f"total_{port}"]
+            ].dropna()
+            count = df_all[f"{port}_{tag}"].__len__()
+            percentage = count / n_route * 100
+            print(f"{count} all {tag} in {port}, {percentage}%.")
+
+
+if __name__ == "__main__":
+    main()
