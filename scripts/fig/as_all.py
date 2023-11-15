f1972026fa3607f304f8206ec0ceaebf5e3fbb34
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-11-15 12:19:04 +0800

    stats for ASes with all reports in the same type;fix stats for routes to consider no import

--- scripts/fig/as_all.py
+++ scripts/fig/as_all.py
@@ -1,22 +1,24 @@
-"""Run at `scripts/` with `python3 -m fig.route_all`.
+"""Run at `scripts/` with `python3 -m fig.as_all`.
 Data are from here:
-<https://github.com/SichangHe/internet_route_verification/issues/88>
+<https://github.com/SichangHe/internet_route_verification/issues/89>
+
+Adopted from `route_all`.
 """
 import pandas as pd
 from fig import download_if_missing
 
-FILE = "route_stats1.csv.gz"
+FILE = "as_stats1.csv"
 PORTS = ("import", "export")
 TAGS = ("ok", "skip", "unrec", "meh", "err")
 
 
 def main():
     download_if_missing(
-        "https://github.com/SichangHe/internet_route_verification/releases/download/data-88/route_stats1.csv.gz",
+        "https://github.com/SichangHe/internet_route_verification/files/13307781/as_stats1.csv",
         FILE,
     )
-    df = pd.read_csv(FILE, dtype="uint16")
-    n_route = len(df)
+    df = pd.read_csv(FILE)
+    n_as = len(df)
     df["total"] = sum((df[f"{port}_{tag}"] for tag in TAGS for port in PORTS))
     df_all = {}
     for tag in TAGS:
@@ -24,7 +26,7 @@ def main():
             df[f"import_{tag}"] + df[f"export_{tag}"] == df["total"]
         ].dropna()
         count = df_all[tag].__len__()
-        percentage = count / n_route * 100
+        percentage = count / n_as * 100
         print(f"{count} all {tag}, {percentage}%.")
     for port in PORTS:
         print()
@@ -32,7 +34,7 @@ def main():
 
         df_all[f"{port}_dne"] = df[df[f"total_{port}"] == 0].dropna()
         count = df_all[f"{port}_dne"].__len__()
-        percentage = count / n_route * 100
+        percentage = count / n_as * 100
         print(f"{count} have no {port}, {percentage}%")
 
         for tag in TAGS:
@@ -41,7 +43,7 @@ def main():
                 & (df[f"{port}_{tag}"] == df[f"total_{port}"])
             ].dropna()
             count = df_all[f"{port}_{tag}"].__len__()
-            percentage = count / n_route * 100
+            percentage = count / n_as * 100
             print(f"{count} all {tag} in {port}, {percentage}%.")
 
 
