4a879df61fa18d5319440f4484691c36b8a82f4f
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-11-15 12:35:54 +0800

    AS stats count against ASes with exchanges

--- scripts/fig/as_all.py
+++ scripts/fig/as_all.py
@@ -19,6 +19,8 @@ def main():
     )
     df = pd.read_csv(FILE)
     n_as = len(df)
+    print(f"{n_as} ASes in total.")
+
     df["total"] = sum((df[f"{port}_{tag}"] for tag in TAGS for port in PORTS))
     df_all = {}
     for tag in TAGS:
@@ -27,15 +29,16 @@ def main():
         ].dropna()
         count = df_all[tag].__len__()
         percentage = count / n_as * 100
-        print(f"{count} all {tag}, {percentage}%.")
+        print(f"{count} all {tag}, {percentage:.2f}%.")
     for port in PORTS:
         print()
         df[f"total_{port}"] = sum((df[f"{port}_{tag}"] for tag in TAGS))
 
         df_all[f"{port}_dne"] = df[df[f"total_{port}"] == 0].dropna()
-        count = df_all[f"{port}_dne"].__len__()
-        percentage = count / n_as * 100
-        print(f"{count} have no {port}, {percentage}%")
+        n_dne = df_all[f"{port}_dne"].__len__()
+        percentage = n_dne / n_as * 100
+        n_e = n_as - n_dne
+        print(f"{n_dne} have no {port}, {percentage:.2f}%; {n_e} have {port}.")
 
         for tag in TAGS:
             df_all[f"{port}_{tag}"] = df[
@@ -43,8 +46,10 @@ def main():
                 & (df[f"{port}_{tag}"] == df[f"total_{port}"])
             ].dropna()
             count = df_all[f"{port}_{tag}"].__len__()
-            percentage = count / n_as * 100
-            print(f"{count} all {tag} in {port}, {percentage}%.")
+            percentage = count / n_e * 100
+            print(
+                f"{count} all {tag} in {port}, {percentage:.2f}% among ASes with {port}."
+            )
 
 
 if __name__ == "__main__":
--- scripts/fig/route_all.py
+++ scripts/fig/route_all.py
@@ -17,6 +17,8 @@ def main():
     )
     df = pd.read_csv(FILE, dtype="uint16")
     n_route = len(df)
+    print(f"{n_route} routes in total.")
+
     df["total"] = sum((df[f"{port}_{tag}"] for tag in TAGS for port in PORTS))
     df_all = {}
     for tag in TAGS:
@@ -25,15 +27,16 @@ def main():
         ].dropna()
         count = df_all[tag].__len__()
         percentage = count / n_route * 100
-        print(f"{count} all {tag}, {percentage}%.")
+        print(f"{count} all {tag}, {percentage:.2f}%.")
     for port in PORTS:
         print()
         df[f"total_{port}"] = sum((df[f"{port}_{tag}"] for tag in TAGS))
 
         df_all[f"{port}_dne"] = df[df[f"total_{port}"] == 0].dropna()
-        count = df_all[f"{port}_dne"].__len__()
-        percentage = count / n_route * 100
-        print(f"{count} have no {port}, {percentage}%")
+        n_dne = df_all[f"{port}_dne"].__len__()
+        percentage = n_dne / n_route * 100
+        n_e = n_route - n_dne
+        print(f"{n_dne} have no {port}, {percentage:.2f}%; {n_e} have {port}.")
 
         for tag in TAGS:
             df_all[f"{port}_{tag}"] = df[
@@ -41,8 +44,10 @@ def main():
                 & (df[f"{port}_{tag}"] == df[f"total_{port}"])
             ].dropna()
             count = df_all[f"{port}_{tag}"].__len__()
-            percentage = count / n_route * 100
-            print(f"{count} all {tag} in {port}, {percentage}%.")
+            percentage = count / n_e * 100
+            print(
+                f"{count} all {tag} in {port}, {percentage:.2f}% among routes with {port}."
+            )
 
 
 if __name__ == "__main__":
