18860437c22859ac88f0454aff34dd63e629a239
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2023-12-27 11:18:52 +0800

    attempt to finish the stats generation and writing to disk

--- scripts/src/main.rs
+++ scripts/src/main.rs
@@ -1,12 +1,17 @@
+#![allow(clippy::unnecessary_lazy_evaluations)]
+use core::cmp::Ordering::*;
 use std::{
+    collections::BTreeMap,
     fs::File,
-    io::{BufRead, BufReader, Read},
+    io::{BufRead, BufReader, BufWriter, Read, Write},
+    sync::{Arc, Mutex},
 };
 
 use anyhow::{Context, Result};
 use flate2::read::GzDecoder;
 use rayon::prelude::*;
 
+#[allow(dead_code)]
 struct CsvFile {
     path: &'static str,
     url: &'static str,
@@ -271,8 +276,144 @@ struct Row {
     export_err: u16,
 }
 
-#[allow(clippy::never_loop)]
-fn process_file<R: Read>(file: BufReader<R>) -> Result<()> {
+impl Row {
+    fn percentages(
+        &self,
+    ) -> (
+        Option<Percentages>,
+        Option<Percentages>,
+        Option<Percentages>,
+    ) {
+        let Self {
+            import_ok,
+            export_ok,
+            import_skip,
+            export_skip,
+            import_unrec,
+            export_unrec,
+            import_meh,
+            export_meh,
+            import_err,
+            export_err,
+        } = self;
+        let (
+            import_ok,
+            export_ok,
+            import_skip,
+            export_skip,
+            import_unrec,
+            export_unrec,
+            import_meh,
+            export_meh,
+            import_err,
+            export_err,
+        ) = (
+            *import_ok as f64,
+            *export_ok as f64,
+            *import_skip as f64,
+            *export_skip as f64,
+            *import_unrec as f64,
+            *export_unrec as f64,
+            *import_meh as f64,
+            *export_meh as f64,
+            *import_err as f64,
+            *export_err as f64,
+        );
+
+        let import_total = import_ok + import_skip + import_unrec + import_meh + import_err;
+        let export_total = export_ok + export_skip + export_unrec + export_meh + export_err;
+        let total = import_total + export_total;
+
+        let import_percentages = (import_total > 0.0).then(|| Percentages {
+            ok: import_ok * 100.0 / import_total,
+            skip: import_skip * 100.0 / import_total,
+            unrec: import_unrec * 100.0 / import_total,
+            meh: import_meh * 100.0 / import_total,
+            err: import_err * 100.0 / import_total,
+        });
+
+        let export_percentages = (export_total > 0.0).then(|| Percentages {
+            ok: export_ok * 100.0 / export_total,
+            skip: export_skip * 100.0 / export_total,
+            unrec: export_unrec * 100.0 / export_total,
+            meh: export_meh * 100.0 / export_total,
+            err: export_err * 100.0 / export_total,
+        });
+
+        let total_percentages = (total > 0.0).then(|| Percentages {
+            ok: (import_ok + export_ok) * 100.0 / total,
+            skip: (import_skip + export_skip) * 100.0 / total,
+            unrec: (import_unrec + export_unrec) * 100.0 / total,
+            meh: (import_meh + export_meh) * 100.0 / total,
+            err: (import_err + export_err) * 100.0 / total,
+        });
+
+        (import_percentages, export_percentages, total_percentages)
+    }
+}
+
+/// None of the fields should be NaN.
+#[derive(Clone, Debug, Default, PartialEq)]
+struct Percentages {
+    ok: f64,
+    skip: f64,
+    unrec: f64,
+    meh: f64,
+    err: f64,
+}
+
+impl Eq for Percentages {}
+
+impl Ord for Percentages {
+    /// Sort by `ok` descending, then `err` ascending, then `skip` descending,
+    /// then `unrec` descending, then `meh` descending.
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        match self
+            .ok
+            .partial_cmp(&other.ok)
+            .expect("`Percentages` should not have NaN")
+        {
+            Equal => {}
+            ord => return ord.reverse(),
+        }
+        match self
+            .err
+            .partial_cmp(&other.err)
+            .expect("`Percentages` should not have NaN")
+        {
+            Equal => {}
+            ord => return ord,
+        }
+        match self
+            .skip
+            .partial_cmp(&other.skip)
+            .expect("`Percentages` should not have NaN")
+        {
+            Equal => {}
+            ord => return ord.reverse(),
+        }
+        match self
+            .unrec
+            .partial_cmp(&other.unrec)
+            .expect("`Percentages` should not have NaN")
+        {
+            Equal => {}
+            ord => return ord.reverse(),
+        }
+        self.meh
+            .partial_cmp(&other.meh)
+            .expect("`Percentages` should not have NaN")
+            .reverse()
+    }
+}
+
+impl PartialOrd for Percentages {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+fn process_file<R: Read>(file: BufReader<R>, stats: Stats) -> Result<()> {
     let mut lines = file.lines();
 
     let _header = lines.next().context("No header")??;
@@ -300,26 +441,115 @@ fn process_file<R: Read>(file: BufReader<R>) -> Result<()> {
             export_err: next_field()?,
         };
 
-        println!("{row:?}");
-        break;
+        let (import_percentages, export_percentages, total_percentages) = row.percentages();
+
+        for (item, pool) in [import_percentages, export_percentages, total_percentages]
+            .into_iter()
+            .zip(&stats)
+        {
+            if let Some(percentages) = item {
+                let mut pool = pool.lock().expect("`pool` should not have been poisoned.");
+                *pool.entry(percentages).or_default() += 1;
+            }
+        }
     }
 
     Ok(())
 }
 
+#[derive(Clone)]
+struct Stats {
+    import: Arc<Mutex<BTreeMap<Percentages, u32>>>,
+    export: Arc<Mutex<BTreeMap<Percentages, u32>>>,
+    total: Arc<Mutex<BTreeMap<Percentages, u32>>>,
+}
+
+impl Stats {
+    fn new() -> Self {
+        Self {
+            import: Arc::new(Mutex::new(BTreeMap::new())),
+            export: Arc::new(Mutex::new(BTreeMap::new())),
+            total: Arc::new(Mutex::new(BTreeMap::new())),
+        }
+    }
+}
+
+impl<'a> IntoIterator for &'a Stats {
+    type Item = &'a Arc<Mutex<BTreeMap<Percentages, u32>>>;
+
+    type IntoIter = std::array::IntoIter<&'a Arc<Mutex<BTreeMap<Percentages, u32>>>, 3>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        [&self.import, &self.export, &self.total].into_iter()
+    }
+}
+
+impl Default for Stats {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 fn main() {
+    let stats = Stats::new();
+
     ROUTE_STATS_ALL
         .par_iter()
         .map(|CsvFile { path, url: _ }| {
             let file = File::open(path).context("Failed to open file")?;
             if path.ends_with(".gz") {
                 let file = BufReader::new(GzDecoder::new(file));
-                process_file(file).with_context(|| format!("Failed to process file {}", path))
+                process_file(file, stats.clone())
+                    .with_context(|| format!("Failed to process file {}", path))
             } else {
                 let file = BufReader::new(file);
-                process_file(file).with_context(|| format!("Failed to process file {}", path))
+                process_file(file, stats.clone())
+                    .with_context(|| format!("Failed to process file {}", path))?;
+                println!("Processed `{}`", path);
+
+                Ok(())
             }
         })
         .collect::<Result<()>>()
         .unwrap();
+
+    for (stats, filename) in stats.into_iter().zip([
+        "route_all_import_stats.csv",
+        "route_all_export_stats.csv",
+        "route_all_total_stats.csv",
+    ]) {
+        let mut file = BufWriter::new(File::create(filename).unwrap());
+        file.write_all(b"%ok,%skip,%unrec,%meh,%err,count\n")
+            .unwrap();
+
+        let stats = stats
+            .lock()
+            .expect("`stats` should not have been poisoned.");
+        for (
+            Percentages {
+                ok,
+                skip,
+                unrec,
+                meh,
+                err,
+            },
+            count,
+        ) in stats.iter()
+        {
+            file.write_all(ok.to_string().as_bytes()).unwrap();
+            file.write_all(b",").unwrap();
+            file.write_all(skip.to_string().as_bytes()).unwrap();
+            file.write_all(b",").unwrap();
+            file.write_all(unrec.to_string().as_bytes()).unwrap();
+            file.write_all(b",").unwrap();
+            file.write_all(meh.to_string().as_bytes()).unwrap();
+            file.write_all(b",").unwrap();
+            file.write_all(err.to_string().as_bytes()).unwrap();
+            file.write_all(b",").unwrap();
+            file.write_all(count.to_string().as_bytes()).unwrap();
+            file.write_all(b"\n").unwrap();
+        }
+
+        file.flush().unwrap();
+    }
 }
