f1d301c84a31ca6ab51db3de2f329f3f0493a8ab
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-01-10 17:11:38 +0800

    successful Zipf fitting after excluding 0s

--- scripts/scripts/stats/as_set_size_fitting.py
+++ scripts/scripts/stats/as_set_size_fitting.py
@@ -8,15 +8,17 @@ FILE = as_set_sizes
 
 def main():
     FILE.download_if_missing()
-    df = pd.read_csv(FILE.path)
-    df_wo_hash = df[~df['as_set'].str.contains('#')]
+    df_raw = pd.read_csv(FILE.path)
+    df_wo_hash = df_raw[~df_raw["as_set"].str.contains("#")]
+    # Only the AS sets with at least one member.
+    df = df_wo_hash[df_wo_hash["size"] > 0]
 
-    res = fit(zipf, df_wo_hash["size"], [(1.0, 10.0)])
+    res = fit(zipf, df["size"], [(1.0, 10.0)])
     print(res)
     (alpha, loc) = res.params
 
     n_bin = 1000
-    max_size = max(df_wo_hash["size"])
+    max_size = max(df["size"])
 
     x = range(1, max_size + 1)
     fitted_data = zipf.pmf(x, alpha, loc=loc)
@@ -24,7 +26,7 @@ def main():
     # Plotting the fitted distribution against the empirical data
     plt.bar(x, fitted_data, alpha=0.5, color="yellow", label="Fitted Zipf Distribution")
     plt.hist(
-        df_wo_hash["size"],
+        df["size"],
         bins=n_bin,
         density=True,
         alpha=0.5,
