9feb05fc68467ecd83a5abd9ee24acb7a0863ca2
Steven Hé (Sīchàng) <sichang.he@dukekunshan.edu.cn>
2024-01-10 17:22:59 +0800

    fit depth as well

--- scripts/scripts/stats/as_set_size_fitting.py
+++ scripts/scripts/stats/as_set_size_fitting.py
@@ -41,5 +41,35 @@ def main():
     plt.title("Fitted Zipf Distribution vs Empirical Data")
     plt.show()
 
+    # Fitting depths.
+    df = df_wo_hash[df_wo_hash["depth"] > 0]
+    res = fit(zipf, df["depth"], [(1.0, 10.0)])
+    print(res)
+    (alpha, loc) = res.params
+
+    n_bin = 1000
+    max_size = max(df["depth"])
+
+    x = range(1, max_size + 1)
+    fitted_data = zipf.pmf(x, alpha, loc=loc)
+
+    # Plotting the fitted distribution against the empirical data
+    plt.bar(x, fitted_data, alpha=0.5, color="yellow", label="Fitted Zipf Distribution")
+    plt.hist(
+        df["depth"],
+        bins=n_bin,
+        density=True,
+        alpha=0.5,
+        color="blue",
+        label="Empirical Data",
+    )
+    plt.legend()
+    plt.xlabel("Value")
+    plt.ylabel("Frequency")
+    plt.xscale("log")
+    plt.yscale("log")
+    plt.title("Fitted Zipf Distribution vs Empirical Data")
+    plt.show()
+
 
 main() if __name__ == "__main__" else None
